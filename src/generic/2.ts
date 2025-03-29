f2a4d815361523f716b77b9e221443c8d94cb51c
ihorborys <ihorborys@gmail.com>
2025-03-29 16:37:32 +0200

    Added task-2, generic

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,7 +11,10 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+type topType = Pick <AllType, 'name' | 'color'>;
+type bottomType = Pick <AllType, 'position' | 'weight'>;
+
+function compare (top: topType, bottom: bottomType): AllType {
   return {
     name: top.name,
     color: top.color,
